ff410332575f5f8cc801391c19751814ee4cc61e
Matt Goldeck <matt@goldeck.net>
2022-08-05 16:03:00 -0400

    adjustments for railway

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import os
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 from clients.rsvp import RSVPClient
 from domain.rsvp import RSVP
-
 
 app = FastAPI()
 
@@ -35,3 +37,6 @@ def submit_rsvp(rsvp: RSVP):
         raise HTTPException(status_code=500, detail="Error submitting RSVP")
     
     return {'success': True}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
